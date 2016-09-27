17896578c89c843b7e40798c24f273fce0f43b46
Archit Srivastava <arch.srivastava.2@gmail.com>
2016-09-26 20:23:31 -0700

    [FEAT] Added choropleth dynamic google maps charts

--- isi/census_data_count.py
+++ isi/census_data_count.py
@@ -25,31 +25,60 @@ db_twitter_posts = db.collection('twitter_posts')
 with open('158-00.geojson', 'r') as f:
 	js = json.load(f)
 
-def count_tract_posts(db_name=db_twitter_posts, field_name='text', query={'coordinates':{'$ne':None}}):
-	db_data = db_name.find(query, {'coordinates.coordinates':1, 'location':1}).limit(10000)
-	idx=0
+def get_value(obj, key):
+	return_val = obj
+	# print key
+	# print return_val
+	for attr in key.split('.'):
+		return_val = return_val[attr]
+	return return_val
+
+def count_tract_posts(type_db, db_name=db_twitter_posts, field_name='text', query={'coordinates':{'$ne':None}}):
+	prop_list = ['arousal', 'dominance', 'valence', 'sentstrength_positive_sentiment', 'sentstrength_negative_sentiment','likes.count', 'comments.count']
+	db_data = db_name.find(query, dict({'coordinates.coordinates':1, 'location':1}.items()+{prop:1 for prop in prop_list}.items()))
 	for data_val in db_data:
 		try:
-			idx=idx+1
-			print idx
-			point = Point(data_val['location']['longitude'], data_val['location']['latitude'])
-			print point
-			# point = Point(data_val['coordinates']['coordinates'][1], data_val['coordinates']['coordinates'][0])
+			try:
+				point = Point(data_val['location']['longitude'], data_val['location']['latitude'])
+			except:
+				try:
+					point = Point(data_val['coordinates']['coordinates'][0], data_val['coordinates']['coordinates'][1])
+				except:
+					pass
 			# check each polygon to see if it contains the point
 			for feature in js['features']:
-				if 'count' not in feature['properties']:
-					feature['properties']['count'] = 0
+				if type_db not in feature['properties']:
+					feature['properties'][type_db] = {}
+				if 'count' not in feature['properties'][type_db]:
+					feature['properties'][type_db]['count'] = 0.0
+				for prop in prop_list:
+					if prop not in feature['properties'][type_db]:
+						feature['properties'][type_db][prop] = 0.0
+						feature['properties'][type_db][prop+'_count'] = 0.0
 				polygon = shape(feature['geometry'])
-				# print polygon
 				if polygon.contains(point):
-					feature['properties']['count'] = feature['properties']['count'] + 1
-					print 'found'
+					for prop in prop_list:
+						try:
+							feature['properties'][type_db][prop] = feature['properties'][type_db][prop] + get_value(data_val, prop)
+							feature['properties'][type_db][prop+'_count'] = feature['properties'][type_db][prop+'_count'] + 1.0
+						except:
+							pass
+					feature['properties'][type_db]['count'] = feature['properties'][type_db]['count'] + 1.0
 		except:
 			# print idx
 			# idx=idx+1
 			pass
-	with open('158-00_changed_instagram.geojson', 'w') as outfile:
-		json.dump(js, outfile)
 
-count_tract_posts(db_instagram_posts, query={})
-# count_tract_posts()
+	for feature in js['features']:
+		for prop in prop_list:
+			try:
+				feature['properties'][type_db][prop+'_avg'] = feature['properties'][type_db][prop] / feature['properties'][type_db][prop+'_count']
+			except:
+				pass
+
+count_tract_posts('twitter_posts')
+count_tract_posts('instagram_posts', db_instagram_posts, query={})
+
+
+with open('tract_data.geojson', 'w') as outfile:
+	json.dump(js, outfile)
